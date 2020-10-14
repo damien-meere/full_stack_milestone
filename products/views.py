f2e0e334613295262e05b78fcc7580e305bb1cdb
Damien Meere <damien.meere@bt.com>
2020-10-14 15:54:35 +0000

    Search bar functionality in place, will search name and description field for search term

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,16 +10,31 @@ def all_products(request):
     """ View to return all products """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search parameters")
+                return redirect(reverse('products'))
+
+            # Q object allows us to bring an OR operation into the search.
+            # This verifies if the search criteria appears in either the name
+            # or the descriptions field.
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """ Return individual product and pass as context to product_detail.html """
+    """ Return individual product, pass to product_detail.html """
 
     product = get_object_or_404(Product, pk=product_id)
 
