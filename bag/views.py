2449f99826ba2e77dd8f1a2c139d282bb075212e
Damien Meere <damien.meere@bt.com>
2020-10-17 13:31:45 +0000

    added update and remove funcitonality to shopping bag page

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -26,7 +26,7 @@ def add_to_bag(request, item_id):
         # if item with the same size, increment.
         # if item has different size, just add to bag
         # else if item isn't already in bag, just add it
-        if item_id in list(bag.keys()): 
+        if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
             else:
@@ -44,3 +44,64 @@ def add_to_bag(request, item_id):
     # overwrite bag dict in session
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity of the specified product to the shopping bag """
+    # gather details from request
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    # session allows browser to store C/S transaction information
+    bag = request.session.get('bag', {})
+
+    # if product being adjusted has a size, need to drill down
+    # and find specific item with that size
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # if quantity is zero - delete item
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        # for items without a size, just set the bag item
+        # quantity to the specified value
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            # if quantity is zero - remove item
+            bag.pop(item_id)
+
+    # overwrite bag dict in session
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove specified product to the shopping bag """
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        # session allows browser to store C/S transaction information
+        bag = request.session.get('bag', {})
+
+        # if product being adjusted has a size, need to drill down
+        # and find specific item with that size and remove it.
+        # if items_by_suze dict empty - remove entire item
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        # overwrite bag dict in session
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
