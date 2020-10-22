b2cff8997945c8ec43d25e86c96d59fb2b3e40ee
Damien Meere <damien.meere@bt.com>
2020-10-22 12:40:14 +0000

    Added form customisation and signals to checkout app

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+
+    # Overriding the ready method and importing our signals module.
+    # Every time a line item is saved or deleted, Our custom update
+    # total model method will be called. Updating the order totals
+    # automatically.
+    def ready(self):
+        import checkout.signals
