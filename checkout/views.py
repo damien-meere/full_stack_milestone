7a8292df9265cb53bfde2685a6ffb9a166a33a33
Damien Meere <damien.meere@bt.com>
2020-10-24 14:20:39 +0000

    Added Stripe card element to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H5v9oII1DOczrG4xAuFZE9jU7t1d7fop6wRBE7Yuw6aMx1stP9VSlb7DQpFSLEZTIK9VhxdBafOkegGlOB5NXCZ00QaagRrTW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
