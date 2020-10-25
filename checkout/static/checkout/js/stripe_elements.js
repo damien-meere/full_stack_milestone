11405e0204a47c02d55d14781bb96eef987e605f
Damien Meere <damien.meere@bt.com>
2020-10-25 14:15:31 +0000

    added stripe environmental variables, and built stripe intent into checkout view

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -26,3 +26,20 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Realtime Validation Errors: everytime there's a change to the card element, check for errors
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        // if an error exists, display within the card erros div on checkout page
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
