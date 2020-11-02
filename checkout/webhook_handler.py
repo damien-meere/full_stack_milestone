45c1a1973708e072d6b774a1a801c40d0e385bf3
Damien Meere <damien.meere@bt.com>
2020-11-02 16:20:34 +0000

    Updated webhook handler to handle user profile information

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -40,6 +41,25 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info is checked
+        profile = None
+        username = intent.metadata.username
+        # if not AnonymousUser user is authenticated, to gather
+        # information
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            # Only update the profile if the save info box is checked
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                # finally, save profile
+                profile.save()
+
         order_exists = False
         attempt = 1
         # try this block 5 times in case there is a delayed response
@@ -77,6 +97,9 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    # unauthenticated users can also create orders with this
+                    # value set to none
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
