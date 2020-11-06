9b09458e6c9ef9048aae61802488e083cee612cb
Damien Meere <damien.meere@bt.com>
2020-11-06 14:37:40 +0000

    edit to stock_level field in product model - now required and defaulting to 0

--- products/models.py
+++ products/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
     stock_level = models.IntegerField(validators=[MinValueValidator(0)],
-                                      null=True, blank=True)
+                                      null=False, default=0)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
