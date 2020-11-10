472f8af5c881dd67609277d629dc40e20dca1997
Damien Meere <damien.meere@bt.com>
2020-11-10 13:13:24 +0000

    Updated SECRET_KEY var to pull directly from environmental variables

--- comic_store/settings.py
+++ comic_store/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p8jl&5f1*4jh4ih-q&yt&2+ob1lg&e_&mngb^doh0w=xdbny27'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dmeere-thecomicstore.herokuapp.com', 'localhost']
 
